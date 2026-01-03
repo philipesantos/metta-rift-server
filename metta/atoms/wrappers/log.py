87a8854f35b9eba55be3d38e7991a66ae97652ff
Philipe Santos <philipe.santos@outlook.com>
2026-01-03 17:46:22 -0300

    added log atom wrapper

--- metta/atoms/wrappers/log.py
+++ metta/atoms/wrappers/log.py
@@ -1,10 +1,11 @@
 class Log:
-    def __init__(self, atom: str):
+    def __init__(self, tick: str, atom: str):
+        self.tick = tick
         self.atom = atom
 
     @staticmethod
-    def to_metta_usage(atom: str):
-        return f"(Log {atom})"
+    def to_metta_usage(tick: str, atom: str):
+        return f"(Log {tick} {atom})"
 
     def to_metta_definition(self) -> str:
-        return f"{self.to_metta_usage(self.atom)}"
+        return f"{self.to_metta_usage(self.tick, self.atom)}"
