da9eaa7cce90d1bf6c6cfc18ff49ce89308c799e
Philipe Santos <philipe.santos@outlook.com>
2026-01-13 18:20:26 -0300

    added compass module

---
+++ modules/compass/compass_module.py
@@ -0,0 +1,15 @@
+from core.definitions.facts.item_fact_definition import ItemFactDefinition
+from core.definitions.wrappers.state_wrapper_definition import StateWrapperDefinition
+from core.patterns.facts.at_fact_pattern import AtFactPattern
+from core.world import World
+from modules.module import Module
+
+
+class CompassModule(Module):
+    def __init__(self, compass_where: str):
+        self.compass_where: str = compass_where
+
+    def apply(self, world: World) -> None:
+        item_compass = ItemFactDefinition("compass")
+        world.add_definition(item_compass)
+        world.add_definition(StateWrapperDefinition(AtFactPattern(item_compass.key, self.compass_where)))
